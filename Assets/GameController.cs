04ad184d60abca8f91e15f0fcb559a13b9f4505f
stumpwimple <stumpwimple@gmail.com>
2024-01-25 11:10:19 -0600

    Initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject selectedObject;
+    public GameObject selectionRing;
+
+    public GameObject FPSCam;
+    public GameObject OrbitCam;
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.V)) {
+            if (OrbitCam.active)
+            {
+                OrbitCam.active = false;
+                FPSCam.active = true;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+               OrbitCam.active = true;
+               FPSCam.active = false;
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Application.Quit();
+        }
+
+
+
+            if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit) && rayHit.collider.gameObject.GetComponent<TurnOffer>())
+            {
+                rayHit.collider.gameObject.GetComponent<TurnOffer>().go.active = !rayHit.collider.gameObject.GetComponent<TurnOffer>().go.active;
+            }
+            else if (Physics.Raycast(ray, out rayHit) && rayHit.collider.gameObject != selectedObject && rayHit.collider.gameObject.name != "Terrain")
+            {
+                selectedObject = rayHit.collider.gameObject;
+                selectionRing.transform.position = new Vector3(selectedObject.transform.position.x, 0, selectedObject.transform.position.z);
+                Debug.Log("You've Selected: " + selectedObject);
+
+            }
+            else if (Physics.Raycast(ray, out rayHit) && rayHit.collider.gameObject == selectedObject)
+            {
+                Camera.main.transform.LookAt(selectedObject.transform);
+                selectedObject = null;
+                selectionRing.transform.position = new Vector3(2, -1, 2);
+                Debug.Log("You've centered View on: " + selectedObject);
+            }
+            else if (Physics.Raycast(ray, out rayHit) && selectedObject && selectedObject.GetComponent<Player>() && rayHit.collider.gameObject.name == "Terrain") {
+                Debug.Log("You've Tapped the terrain at: " + rayHit.point);
+                GameObject moveTarget = new GameObject();
+                moveTarget.transform.position = rayHit.point;
+                selectedObject.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>().target = moveTarget.transform;
+                selectedObject = null;
+                selectionRing.transform.position = new Vector3(2, -1, 2);
+            }
+            else
+            {
+                selectedObject = null;
+                selectionRing.transform.position = new Vector3(2, -1, 2);
+                Debug.Log("You've Deselected Object");
+            }
+        }
+    }
+}
---
+++ Assets/OrbittingCamera.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbittingCamera : MonoBehaviour
+{
+    public GameController gameController;
+    public GameObject cameraTarget;
+    public float rotationSpeed;
+
+    private Vector3 targetCenter;
+    // Use this for initialization
+    void Start()
+    {
+        if (cameraTarget)
+        {
+            targetCenter = cameraTarget.transform.position;
+        }
+        else
+        {
+            targetCenter = this.transform.position;
+        }
+
+        transform.LookAt(targetCenter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController && gameController.selectedObject) {
+            targetCenter = gameController.selectedObject.transform.position;            
+        }
+
+
+        if (Input.GetMouseButton(2) && (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftShift)))
+        {
+            transform.RotateAround(targetCenter, Vector3.up, Input.GetAxis("Mouse X") * rotationSpeed);
+            float xRotation = transform.localRotation.eulerAngles.x;
+            if (xRotation > 180)
+            {
+                xRotation -= 360f;
+            }
+
+            float yMouseAxis = Input.GetAxis("Mouse Y");
+
+            if (xRotation < 80 && xRotation > -80)
+            {
+                transform.RotateAround(targetCenter, -Camera.main.transform.right, yMouseAxis * rotationSpeed);
+            }
+            else if ((xRotation >= 80 && yMouseAxis > 0) || (xRotation <= -80 && yMouseAxis < 0))
+            {
+                transform.RotateAround(targetCenter, -Camera.main.transform.right, yMouseAxis * rotationSpeed);
+            }
+        }
+        else
+        if (Input.GetMouseButton(2))
+        {
+            Debug.Log("Camera up" + Camera.main.transform.up);
+            Debug.Log("Camera right" + Camera.main.transform.right);
+            float yMouseAxis = Input.GetAxis("Mouse Y");
+            float xMouseAxis = Input.GetAxis("Mouse X");
+
+            transform.position -= Camera.main.transform.up * yMouseAxis + Camera.main.transform.right* xMouseAxis;
+
+            //transform.Translate(Camera.main.transform.up * yMouseAxis);
+            //transform.Translate(Camera.main.transform.right * xMouseAxis);
+        }
+
+        float zoomScroll = Input.GetAxis("Mouse ScrollWheel");
+        float speedZoom=1;
+        if(Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift)) { speedZoom = 3; }
+        else { speedZoom = 1; }
+        if (zoomScroll!=0&&Camera.main.orthographic) {
+            Camera.main.orthographicSize -= zoomScroll*10f*speedZoom;
+        }
+        if (zoomScroll != 0 && !Camera.main.orthographic)
+        {
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView-zoomScroll * 10f * speedZoom, 5f, 75);
+        }
+    }
+}
